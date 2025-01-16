d4059d789317ebfc37d17870ab1c430d1dc0bb9f
Biasiolo <biasiolorafael@gmail.com>
2025-01-16 01:29:23 -0300

    teste 5

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -28,14 +28,14 @@ function Game() {
 
   return (
     <Container fluid className="d-flex flex-column vh-100">
-      <Row className="flex-grow-1">
+      <Row className="flex-grow-1 mt-4">
         <Col className="d-flex flex-column justify-content-center align-items-center">
           <Narrative message={message} />
           <p className="text-success mt-3">{motivation}</p> {/* Mensagem motivacional */}
-          <ProgressBar now={progress} label={`${Math.round(progress)}%`} className="w-75 my-4" />
+          <ProgressBar now={progress} label={`${Math.round(progress)}%`} className="w-75 my-2" />
         </Col>
       </Row>
-      <Row className="bg-light border-top mb-4">
+      <Row className="bg-light border-top my-4">
         <Col>
           <CardDeck cards={cards} onAnswer={handleAnswer} />
         </Col>
